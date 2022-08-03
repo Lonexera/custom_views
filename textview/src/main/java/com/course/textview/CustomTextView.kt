59b0e220cf18fd5722526f5a279e3171e7d670e1
Tatsiana.Talkachova <Tatsiana.Talkachova@itechart-group.com>
2022-08-03 23:21:58 +0300

    save custom view state

--- textview/src/main/java/com/course/textview/CustomTextView.kt
+++ textview/src/main/java/com/course/textview/CustomTextView.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.os.Parcelable
 import android.text.StaticLayout
 import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
+import kotlinx.parcelize.Parcelize
 import kotlin.properties.Delegates.notNull
 
 class CustomTextView(
@@ -15,47 +17,97 @@ class CustomTextView(
     attrs: AttributeSet
 ) : View(context, attrs) {
 
-    private val attributes = context.theme.obtainStyledAttributes(
-        attrs,
-        R.styleable.CustomTextView,
-        0,
-        0
+    @Parcelize
+    private data class Parameters(
+        val text: String?,
+        val textSize: Int
+    ) : Parcelable
+
+    @Parcelize
+    private data class SaveState(
+        val superSavedState: Parcelable?,
+        val parameters: Parameters
+    ) : View.BaseSavedState(superSavedState), Parcelable
+
+    private var parameters: Parameters = Parameters(
+        text = null,
+        textSize = DEFAULT_TEXT_SIZE
     )
-    private val text = attributes.getString(R.styleable.CustomTextView_ctv_text)
-    private val attrsTextSize =
-        attributes.getInt(R.styleable.CustomTextView_ctv_textSize, DEFAULT_TEXT_SIZE)
+
+    init {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.CustomTextView,
+            0,
+            0
+        )
+            .apply {
+                try {
+                    parameters = parameters.copy(
+                        text = getString(R.styleable.CustomTextView_ctv_text),
+                        textSize = getInt(R.styleable.CustomTextView_ctv_textSize, DEFAULT_TEXT_SIZE)
+                    )
+                } finally {
+                    recycle()
+                }
+            }
+    }
 
     private val textPaint = TextPaint().apply {
         color = Color.BLACK
         textAlign = Paint.Align.LEFT
-        textSize = attrsTextSize * resources.displayMetrics.scaledDensity
+        textSize = parameters.textSize * resources.displayMetrics.scaledDensity
     }
 
     private var staticLayout: StaticLayout by notNull()
 
+    override fun onSaveInstanceState(): Parcelable {
+        return SaveState(
+            superSavedState = super.onSaveInstanceState(),
+            parameters = parameters
+        )
+    }
 
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        val savedState = state as? SaveState
+        super.onRestoreInstanceState(savedState?.superSavedState ?: state)
+
+        savedState ?: return
+        parameters = savedState.parameters
+    }
+    
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        if (text == null) return
+        if (parameters.text == null) {
+            setMeasuredDimension(widthMeasureSpec, heightMeasureSpec)
+            return
+        } else {
+            val text = requireNotNull(parameters.text)
 
-        val desiredWidth = textPaint.measureText(text).toInt()
+            val measuredWidth = textPaint.measureText(text).toInt()
 
-        val width = resolveSize(desiredWidth, widthMeasureSpec)
-        staticLayout = StaticLayout.Builder
-            .obtain(text, 0, text.length, textPaint, width)
-            .build()
+            val width = resolveSize(measuredWidth, widthMeasureSpec)
+            staticLayout = StaticLayout.Builder
+                .obtain(text, 0, text.length, textPaint, width)
+                .build()
 
-        val height = resolveSize(staticLayout.height, heightMeasureSpec)
+            val height = resolveSize(staticLayout.height, heightMeasureSpec)
 
-        // To avoid issues in layout preview
-        if (width < 0 || height < 0) return
-
-        setMeasuredDimension(width, height)
+            setMeasuredDimension(width, height)
+        }
     }
 
     override fun onDraw(canvas: Canvas?) {
-        if (canvas == null || text == null) return
+        if (canvas == null || parameters.text == null) return
 
         staticLayout.draw(canvas)
+    }
+
+    fun setText(text: String) {
+        parameters = parameters.copy(
+            text = text
+        )
+        requestLayout()
+        invalidate()
     }
 
     companion object {
