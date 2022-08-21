8a37399255fe745b32cb42ff63472dfa1770cc26
Tatsiana.Talkachova <Tatsiana.Talkachova@itechart-group.com>
2022-08-21 21:58:38 +0300

    add spacing attribute

--- stacklinearlayout/src/main/java/com/course/stacklinearlayout/StackLinearLayout.kt
+++ stacklinearlayout/src/main/java/com/course/stacklinearlayout/StackLinearLayout.kt
@@ -3,6 +3,7 @@ package com.course.stacklinearlayout
 import android.content.Context
 import android.util.AttributeSet
 import android.view.ViewGroup
+import androidx.core.content.withStyledAttributes
 import androidx.core.view.children
 
 class StackLinearLayout @JvmOverloads constructor(
@@ -11,12 +12,28 @@ class StackLinearLayout @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : ViewGroup(context, attrs, defStyleAttr) {
 
+    private var spacing: Int = DEFAULT_SPACING_SIZE
+
+    init {
+        context.withStyledAttributes(
+            attrs,
+            R.styleable.StackLinearLayout,
+            0
+        ) {
+            spacing = getDimensionPixelSize(
+                R.styleable.StackLinearLayout_spacing,
+                DEFAULT_SPACING_SIZE
+            )
+        }
+    }
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         measureChildren(widthMeasureSpec, heightMeasureSpec)
 
         val measuredHeight = when (MeasureSpec.getMode(heightMeasureSpec)) {
             MeasureSpec.EXACTLY -> heightMeasureSpec
-            MeasureSpec.AT_MOST, MeasureSpec.UNSPECIFIED -> children.sumOf { it.measuredHeight }
+            MeasureSpec.AT_MOST, MeasureSpec.UNSPECIFIED ->
+                children.sumOf { it.measuredHeight } + spacing * (childCount - 1)
             else -> error("Unreachable")
         }
 
@@ -43,7 +60,18 @@ class StackLinearLayout @JvmOverloads constructor(
                     right,
                     top + layoutHeight
                 )
+
+                layoutHeight += spacing
             }
         }
+    }
+
+    fun setSpacing(sizePx: Int) {
+        spacing = sizePx
+        invalidate()
+    }
+
+    companion object {
+        private const val DEFAULT_SPACING_SIZE = 0
     }
 }
